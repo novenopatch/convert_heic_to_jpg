044a001d1496fcf3c5db2fa76a47286f7d2a0ebc
novenopatch <novenosexta77@gmail.com>
2023-06-08 18:02:39 +0000

    init

---
+++ readme.md
@@ -0,0 +1,35 @@
+# HEIC to JPG Converter
+
+This Python script allows you to convert HEIC files to JPG format. It uses the PIL module to perform the conversion.
+
+## Prerequisites
+
+- Python 3.x
+- PIL module (installed via `pip install pillow`)
+
+## Usage
+
+1. Make sure you have Python 3.x and the PIL module installed on your system.
+2. Place the HEIC files you want to convert into a folder.
+3. Open a command line interface and run the script using one of the following commands:
+
+4. The converted JPG files will be created in the same folder as the original files.
+
+**Note:** If no output path is specified, the script will create the converted JPG files in the same folder as the HEIC files.
+
+## Disclaimer
+
+Make sure you have the necessary rights to read the original HEIC files and write the converted JPG files.
+
+---
+
+Feel free to customize this README by adding more details or providing additional examples based on your specific needs.
+
+
+
+##   To convert a single HEIC file to JPG:
+
+python script.py path/to/file.heic -o path/to/file.jpg
+To convert all HEIC files in a folder to JPG:
+python script.py path/to/folder
+
---
+++ setup.py
@@ -0,0 +1,53 @@
+import argparse
+from PIL import Image
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+def convert_heic_to_jpg(heic_path, jpg_path):
+    image = Image.open(heic_path)
+    image.save(jpg_path, "JPEG")
+
+def batch_convert_heic_to_jpg(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".heic") or filename.endswith(".HEIC"):
+            heic_path = os.path.join(folder_path, filename)
+            jpg_path = os.path.join(folder_path, os.path.splitext(filename)[0] + ".jpg")
+            #print(jpg_path)
+            convert_heic_to_jpg(heic_path, jpg_path)
+
+def convert_with_gui():
+    root = tk.Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory(title="Sélectionnez le dossier contenant les fichiers HEIC")
+    if folder_path:
+        batch_convert_heic_to_jpg(folder_path)
+        print("Conversion en lot réussie !")
+    else:
+        print("Aucun dossier sélectionné.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convertisseur HEIC en JPG")
+    parser.add_argument("input", help="Chemin vers le fichier HEIC ou le dossier")
+    parser.add_argument("-o", "--output", help="Chemin vers le fichier JPG de sortie ou le dossier")
+    parser.add_argument("--gui", action="store_true", help="Lancer l'interface graphique pour la sélection du dossier")
+    args = parser.parse_args()
+
+    if args.gui:
+        convert_with_gui()
+    else:
+        input_path = args.input
+        output_path = args.output
+
+        if os.path.isfile(input_path):
+            if not output_path:
+                output_path = os.path.splitext(input_path)[0] + ".jpg"
+            convert_heic_to_jpg(input_path, output_path)
+            print("Conversion réussie !")
+        elif os.path.isdir(input_path):
+            if not output_path:
+                output_path = input_path
+            batch_convert_heic_to_jpg(input_path)
+            print("Conversion en lot réussie !")
+        else:
+            print("Chemin d'entrée invalide.")
